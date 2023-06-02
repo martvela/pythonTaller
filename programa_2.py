26fe6140df3b82379d3aa2c2349d919c125a0629
Jesus Santiago <jesusantiago27@gmail.com>
2023-06-02 15:46:10 -0600

    agregamos estos archivos de la clase 3

--- programa_2.py
+++ programa_2.py
@@ -1,5 +1,9 @@
-precioDeManzana:int=10
-cantidadManzanas:int=5
+precioDeManzana:int = int(input("Ingrese el precio de la manzana: "))
+cantidadManzanas:int = int(input("Ingrese la cantidad de las manzanas: "))
 #Comentarios de una linea 
-print("Vas a pagar ")
-print(cantidadManzanas * precioDeManzana)
+
+#Concatenacion diferentes formas 
+print("Las manzanas estan en: " + str(precioDeManzana))
+print("La cantidad de manzanas es:", cantidadManzanas)
+print(f"Vas a pagar: {cantidadManzanas * precioDeManzana}")
+
