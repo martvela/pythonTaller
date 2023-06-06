8fa870c5fb0672f8007d99cbbf9bb19fe99d5d79
Jesus Santiago <jesusantiago27@gmail.com>
2023-06-05 20:54:59 -0600

    Para guardar el archivo de dolares a pesos

---
+++ pesotodollar.py
@@ -0,0 +1,7 @@
+print("Conversor de dolares a pesos")
+print("========================================")
+dollar = float(input("Inrese la cantidad en dolares:"))
+pesos:float=17.46
+
+print(dollar, " dolares es igual a " , pesos*dollar, " pesos")
+
