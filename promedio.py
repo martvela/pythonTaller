418d137912bea85f36914e1b9c629cf8d709f30d
Jesus Santiago <jesusantiago27@gmail.com>
2023-06-13 19:50:11 -0600

    promedio

---
+++ promedio.py
@@ -0,0 +1,17 @@
+contador=0
+califs=[]
+def promedio(valores=[]):
+    media= sum(valores) / len(valores)
+    return media 
+
+while contador < 10:
+    valor=int(input(f"Ingrese la calificación del alumno {contador+1}: "))
+    califs.append(valor)
+    contador+=1
+print(f"Las calificaciones guardadas son: {califs}")
+
+
+print(f"El promedio de la clase es: {promedio(califs)}")
+
+
+    
